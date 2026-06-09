f8fb0f047fc8e411fcba58d43f8ffc31c813e1b6
Kerem Örer <161973110+thecubedaryl@users.noreply.github.com>
2026-06-09 22:31:35 +0300

    Update constants.py

--- pf_localization/constants.py
+++ pf_localization/constants.py
@@ -7,14 +7,14 @@ import numpy as np
 
 # 2-D tag positions in world frame [m]
 TAG_POSITIONS = np.array([
-    (-1.50, -2.46),   # tag 0  S1
-    ( 1.20, -2.46),   # tag 1  S2
-    (-0.30,  2.46),   # tag 2  N1
-    ( 2.00,  2.46),   # tag 3  N2
-    (-2.96, -0.50),   # tag 4  W1
-    (-2.96,  1.50),   # tag 5  W2
-    ( 2.96,  0.50),   # tag 6  E1
-    ( 2.96, -1.50),   # tag 7  E2
+    (-2.10, -2.46),   # tag 0  S1 – South wall,
+    ( 0.80, -2.46),   # tag 1  S2 – South wall, 
+    (-0.70,  2.46),   # tag 2  N1 – North wall,
+    ( 2.30,  2.46),   # tag 3  N2 – North wall,
+    (-2.96, -1.80),   # tag 4  W1 – West wall,
+    (-2.96,  1.10),   # tag 5  W2 – West wall, 
+    ( 2.96,  0.20),   # tag 6  E1 – East wall, 
+    ( 2.96, -0.50),   # tag 7  E2 – East wall,
 ], dtype=float)
 
 TAG_Z    = 1.00   # tag center height [m]
